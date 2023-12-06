8b2a37afd44c2a62780b68e52e07844f43cd9046
desertfoox <salar.nili097@gmail.com>
2023-12-06 11:47:11 +0330

    add input form component

---
+++ src/components/common/inputForm.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+interface IInputProps{
+    label:string,
+    labelClassName?:string,
+    type:string,
+    className:string
+}
+const InputForm: React.FC<IInputProps> = ({label,labelClassName,type,className}) => {
+    return (
+        <div>
+            <label
+                htmlFor={label}
+                className={`block text-sm font-medium leading-6 text-gray-900 ${labelClassName}`}
+            >
+                {label}
+            </label>
+            <div className="mt-2">
+                <input
+                    id={label}
+                    name={label}
+                    type={type}
+                    autoComplete="current-password"
+                    className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 ${className}`}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default InputForm
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,4 +1,5 @@
 import logo from '../../assets/images/logo/logo.png'
+import InputForm from '../../components/common/inputForm'
 
 const Login = () => {
     return (
@@ -16,49 +17,15 @@ const Login = () => {
                 </div>
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
                     <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-                        <form className="space-y-6" action="#" method="POST">
-                            <div>
-                                <label
-                                    htmlFor="email"
-                                    className="block text-sm font-medium leading-6 text-gray-900"
-                                >
-                                    Email
-                                </label>
-                                <div className="mt-2">
-                                    <input
-                                        id="email"
-                                        name="email"
-                                        type="email"
-                                        autoComplete="email"
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    />
-                                </div>
-                            </div>
-                            <div>
-                                <label
-                                    htmlFor="password"
-                                    className="block text-sm font-medium leading-6 text-gray-900"
-                                >
-                                    Password
-                                </label>
-                                <div className="mt-2">
-                                    <input
-                                        id="password"
-                                        name="password"
-                                        type="password"
-                                        autoComplete="current-password"
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-red-500 sm:text-sm sm:leading-6"
-                                        />
-                                </div>
-                            </div>
-                            <div>
-                                <button
-                                    type="submit"
-                                    className="flex w-full justify-center rounded-md bg-[#0099CC] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                >
-                                    Sign in
-                                </button>
-                            </div>
+                        <form className="space-y-6" >
+                            <InputForm label={'Username'} type={'text'} className={''} />
+                            <InputForm label={'password'} type={'password'} className={''} />
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-[#0099CC] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Sign in
+                            </button>
                         </form>
                         <div>
 
