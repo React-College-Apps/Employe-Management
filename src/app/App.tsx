b7d903232c86c7c605b7a4047f97259c9f109b46
desertfoox <salar.nili097@gmail.com>
2023-12-06 10:48:43 +0330

    add react router dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Login from '../pages/login/login';
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/container/main.tsx
+++ src/container/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../app/App.tsx'
-import './assets/css/App.css'
+import '../assets/css/App.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
