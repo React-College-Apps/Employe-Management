21b4d5e3f5357fcbd22ca7b9d93bd7b42a939cfb
desertfoox <salar.nili097@gmail.com>
2023-12-07 11:35:20 +0330

    add user data and interface

---
+++ src/core/interface/IUserInterface.ts
@@ -0,0 +1,11 @@
+interface IUserInterface {
+    id:any,
+    isChecked: any
+    name: string
+    position: string
+    email: string
+    role: string
+    isActive: boolean
+  }
+
+export default IUserInterface
