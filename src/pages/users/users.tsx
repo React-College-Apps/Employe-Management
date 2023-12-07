e5986c0e22ac4574b6a919e3dd51a613080a4ddf
Salar Nili <77540189+DesertFoox@users.noreply.github.com>
2023-12-07 14:31:10 +0330

    add pagination

--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -20,8 +20,8 @@ const Users = () => {
     const [sortByName, setSortByName] = useState('');
     const [sortByTitle, setSortByTitle] = useState('');
     const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(10);
-    
+    const [itemsPerPage] = useState(10);
+
     const toggleAllCheckboxes = () => {
         const updatedUsers = users.map(user => ({ ...user, isChecked: !allChecked }));
         setUsers(updatedUsers);
@@ -48,14 +48,36 @@ const Users = () => {
     };
 
     const getSortedUsers = () => {
-        return [...users].sort((a, b) => {
+        return [...currentItems].sort((a, b) => {
             if (sortByName) return compareUsers(a, b, 'name', sortByName);
             if (sortByTitle) return compareUsers(a, b, 'position', sortByTitle);
             return 0;
         });
     };
 
-    
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = users.slice(indexOfFirstItem, indexOfLastItem);
+
+    const totalPages = Math.ceil(users.length / itemsPerPage);
+
+    const renderPagination = () => {
+        const pageNumbers = [];
+        for (let i = 1; i <= totalPages; i++) {
+            pageNumbers.push(i);
+        }
+
+        return pageNumbers.map(number => (
+            <button
+                key={number}
+                onClick={() => setCurrentPage(number)}
+                className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 ${currentPage === number ? 'bg-[#0099CC] text-white' : ''}`}
+            >
+                {number}
+            </button>
+        ));
+    };
+
     return (
         <DashboardLayout>
             <h1>Users</h1>
@@ -72,7 +94,7 @@ const Users = () => {
                             <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                                 <button
                                     type="button"
-                                    className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    className="block rounded-md bg-[#0099CC] px-3 py-2 text-center text-sm font-semibold text-white shadow-sm   "
                                 >
                                     Add user
                                 </button>
@@ -90,6 +112,21 @@ const Users = () => {
                                 onCheckAll={toggleAllCheckboxes}
                                 onCheck={toggleCheckbox}
                             />
+                            <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+                                <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                                    <div>
+                                        <p className="text-sm text-gray-700">
+                                            Showing <span className="font-medium">{indexOfFirstItem + 1}</span> to <span className="font-medium">{indexOfLastItem}</span> of{' '}
+                                            <span className="font-medium">{users.length}</span> results
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                                            {renderPagination()}
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
