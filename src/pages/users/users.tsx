4867e5eae8f0cc103662bafdebe038dc786fdb54
Salar Nili <77540189+DesertFoox@users.noreply.github.com>
2023-12-10 11:55:11 +0330

    add action component

--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -16,6 +16,7 @@ import { setItem } from '../../core/storage/storage'
 import { useUsers } from '../../context/User.Context'
 import SelectInput from '../../components/common/selectInput'
 import Button from '../../components/common/button'
+import Actions from '../../components/users/actions/actions'
 
 
 const Users = () => {
@@ -264,19 +265,7 @@ const Users = () => {
                                 onSearchChange={searchInTableHandler}
                                 sortByAge={sortByAge}
                                 setSortByAge={setSortByAgeHandler} />
-                            <div >
-                                <h2 className='mb-3'>Action</h2>
-                                <div className='grid grid-cols-3 justify-center items-center'>
-                                    <div>
-                                        <SelectInput className="mb-3" onChange={(value) => setUserAction(value)} title={'User Actions'} options={actionOptions} />
-                                    </div>
-                                    <div>
-                                        <Button className=" bg-yellow-500 hover:bg-yellow-400 text-xs mt-5 ml-3 px-3 py-2" onClick={() => actionHandler()}> Submit Action</Button>
-                                    </div>
-
-
-                                </div>
-                            </div>
+                            <Actions setUserAction={setUserAction} actionHandler={actionHandler} actionOptions={actionOptions} />
 
                             <UserTable
                                 users={currentUsers}
